f3f5322bd00bd427fe91727d602e2f65699ae1d4
Augusto Pasinatto <pasinatto.augusto.br@gmail.com>
2023-09-27 09:42:15 -0300

    Add-new-files

---
+++ README.md
@@ -0,0 +1,22 @@
+<h1>🧩Projeto One</h1>
+
+<h2>Descrição Geral</h2>
+Vamos desenvolver nosso primeiro site!
+Neste projeto, aplicaremos conhecimentos de CSS, HTML e JavaScript, bem como práticas comuns de Git e GitHub, buscando a construção de um site que irá gerar os papers customizados para criação de personagens de **RPG**.
+
+<h2>💎Objetivos</h2>
+
+* **Documentar** nosso progresso como Devs
+* **Aprender** na prática boas técnicas de organização em equipe
+* **Expandir** conhecimento na área
+* Se **divertir**, tornando o processo de aprendizagem agradável e gratificante
+
+
+<h2>🫱🏼‍🫲🏽Autores</h2>	  
+
+- <h3>Augusto Pasinatto</h3>
+
+- <h3>Alexandre Veadrigo Júnior</h3>
+
+- <h3>Guilherme Gabriel Fochezatto</h3>
+
---
+++ landing_homepage_logics.js
@@ -0,0 +1,68 @@
+/*passagem dos elementos HTML para o JS*/
+const startButton = document.getElementById('button-start');
+const titulo = document.getElementById('titulo');
+const homeview = document.getElementById('home-view');
+const container = document.getElementById('container-centro');
+
+/*eventos ao clicar o botão 'Começar Aventura!' */
+startButton.addEventListener('click', function() {
+
+    /* tira a visualização do botao e do titulo */
+    this.style.display = 'none';
+    titulo.style.display = 'none';
+    
+    /* cria um novo título identico ao anterior*/
+    const title = document.createElement("div");
+    title.innerHTML = '<h1 id="titulo">AUTO D&D</h1>';
+    homeview.appendChild(title);
+
+    /* cria a GUI de login */
+    const buttons = document.createElement("div");
+    buttons.innerHTML = `
+    <h2>Usuário</h2>
+    <input type="text" id="user">
+    <br>
+    <br> 
+    <h2>Senha</h2>
+    <input type="password" id="pass">
+    <br>
+    <input type="button" id="login" value="LOGIN">
+    <input type="button" id="register" value="REGISTRE-SE">
+    `;
+    container.appendChild(buttons);
+
+    /* função que será chamada ao momento do login (função para testar os botões e o pressionar da tecla enter)*/
+    function escondeElementos() {
+        homeview.style.animation = 'fade-out 0.5s'; //frescura 1
+        homeview.style.animationFillMode = 'forwards'; //frescura 2
+        setTimeout(function() {
+            homeview.addEventListener('animationend', (event) => {homeview.style.visibility = 'hidden'}); //Some da tela após acabar a frescura
+        }, 200);
+    }
+
+    /* aqui são os argumentos relacionados aos botões da nova UI */
+    /* está em time-out pois não identifica os elementos recém criados se carregado junto com clique do botão "Start" */
+    setTimeout(function() {
+        const login = document.getElementById('login');
+        const pass = document.getElementById('pass');
+        const register = document.getElementById('register');
+
+            pass.addEventListener('keydown', function(event) {
+            if (event.key === "Enter") {
+                event.preventDefault(); // Evita o envio do formulário
+                escondeElementos();
+                }
+            });
+        
+            login.addEventListener('click', function() {
+                escondeElementos();
+            });
+
+            register.addEventListener('click', function() {
+                escondeElementos();
+            });
+
+    }, 100);
+
+});
+
